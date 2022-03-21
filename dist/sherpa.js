9c24b9d5c0b674cdf16095c1ce5bac5229344972
Mark Camball <mark.camball@gmail.com>
2022-03-21 17:26:09 -0400

    build

--- dist/sherpa.js
+++ dist/sherpa.js
@@ -925,7 +925,7 @@ var SherpaSDK = class {
     return compliance;
   }
   async sendDeposit(valueWei, commitment, selectedToken, fromAddress) {
-    const sherpaProxyAddress = getters.getSherpaProxyContract(this.netId);
+    const sherpaProxyAddress = getters.getSherpaProxyContract(this.chainId);
     if (this.chainId !== await this.web3.eth.getChainId()) {
       throw new Error("Cant make a deposit in wrong network");
     }
